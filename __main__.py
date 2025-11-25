4b0bec5b36947c0082248443b4c63114cd577c05
is <scaner@gmail.com>
2025-11-25 17:13:05 +0800

    refactor: update app import and enhance hypercorn config

--- __main__.py
+++ __main__.py
@@ -2,14 +2,16 @@ from hypercorn.asyncio import serve
 import hypercorn
 import asyncio
 
-from shapi.api import app
-
+from shapi.app import app
 
 def main():
     """Entry point for running the app."""
     config = hypercorn.Config()
     config.bind = ["0.0.0.0:9080"]
-    asyncio.run(serve(app, config))
+    config.use_reloader = True
+    config.worker_class = "uvloop"
+    config.workers = 1
+    asyncio.run(serve(app, config)) # type: ignore
 
 if __name__ == "__main__":
     main()
